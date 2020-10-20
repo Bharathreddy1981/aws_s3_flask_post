5217351f218e6f3ce95e30ddf9eed4c9c06dad84
Bharathreddy1981 <70434245+Bharathreddy1981@users.noreply.github.com>
2020-10-20 17:46:02 +0530

    change

--- aws_flask.py
+++ aws_flask.py
@@ -10,12 +10,12 @@ aws_flask = Flask(__name__)
 
 @aws_flask.route("/mountain", methods=["POST"])
 def gun():
-    jsondata=request.get_json()
-    final = aws_s3_image.cat(jsondata)
+
+    final = aws_s3_image.cat()
     return jsonify(final)
 
 
 
 
 if __name__=="__main__":
-    aws_flask.run(debug=True)
+    aws_flask.run(host='0.0.0.0')
